0259135fdadb4c4ec3ba22aa85be070c4ff25d0c
nick-bugxhunter <138686129+nick-bugxhunter@users.noreply.github.com>
2024-08-15 13:03:44 +1000

    Update 👋_Welcome.py

--- 👋_Welcome.py
+++ 👋_Welcome.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2024, Matthew Adams
+Copyright (C) 2024, Nick Forstein
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
